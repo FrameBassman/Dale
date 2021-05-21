062a798303bc80acedab1ef5d7d4858a21bd8ecf
Dmitry Romashov <framebassman@gmail.com>
2021-05-22 02:24:37 +0300

    add logs and spring deps

--- application/src/main/java/tech/romashov/dale/application/App.java
+++ application/src/main/java/tech/romashov/dale/application/App.java
@@ -3,12 +3,20 @@
  */
 package tech.romashov.dale.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
 public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+        log.info(new App().getGreeting());
     }
 }
