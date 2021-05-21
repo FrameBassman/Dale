3c436dc8f1e025e17ea1a6ee12c13f41fc5829b0
Dmitry Romashov <framebassman@gmail.com>
2021-05-22 02:28:00 +0300

    start spring

--- application/src/main/java/tech/romashov/dale/application/App.java
+++ application/src/main/java/tech/romashov/dale/application/App.java
@@ -6,7 +6,10 @@ package tech.romashov.dale.application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
@@ -17,6 +20,6 @@ public class App {
     public static void main(String[] args) {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
-        log.info(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
