94bfa8327caf31bddbf8f050d066df4319592c89
Dmitry Romashov <framebassman@gmail.com>
2021-08-30 16:11:48 +0300

    Add validation tests

---
+++ application/web/src/test/java/tech/romashov/dale/application/web/retails/ValidationTests.java
@@ -0,0 +1,28 @@
+package tech.romashov.dale.application.web.retails;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class ValidationTests {
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Test
+    public void validateVendor() throws Exception {
+        expectedEx.expect(RetailException.class);
+        expectedEx.expectMessage("Parameter vendor is null or empty");
+
+        RetailService service = new RetailService();
+        service.add("", "1.1.1.1");
+    }
+
+    @Test
+    public void validateInetAddress() throws Exception {
+        expectedEx.expect(RetailException.class);
+        expectedEx.expectMessage("Parameter ip is null or empty");
+
+        RetailService service = new RetailService();
+        service.add(Vendors.ALL, "");
+    }
+}
