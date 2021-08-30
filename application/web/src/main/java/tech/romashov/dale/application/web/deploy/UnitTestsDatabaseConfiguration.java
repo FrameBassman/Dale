adc2a1cdbbe246da1281c738a43f4487e59cbd7f
Dmitry Romashov <framebassman@gmail.com>
2021-08-30 13:44:11 +0300

    Add h2

---
+++ application/web/src/main/java/tech/romashov/dale/application/web/deploy/UnitTestsDatabaseConfiguration.java
@@ -0,0 +1,20 @@
+package tech.romashov.dale.application.web.deploy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class UnitTestsDatabaseConfiguration {
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
+        basicDataSource.setDriverClassName("org.h2.Driver");
+        basicDataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        basicDataSource.setUsername("sa");
+        basicDataSource.setPassword("sa");
+        return basicDataSource;
+    }
+}
