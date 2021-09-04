d033672b30b6c78717faec941da8398f148749bc
Dmitry Romashov <framebassman@gmail.com>
2021-09-04 09:39:11 +0300

    Add autowired logger

--- application/web/src/main/java/tech/romashov/dale/application/web/HomeController.java
+++ application/web/src/main/java/tech/romashov/dale/application/web/HomeController.java
@@ -1,5 +1,7 @@
 package tech.romashov.dale.application.web;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    private Logger logger;
     @GetMapping
     public String home() {
+        logger.info("Get home page");
         return "It works!";
     }
 }
